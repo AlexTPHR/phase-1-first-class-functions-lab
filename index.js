28ac750deb67b41f452db72b7c06b549c661ed2a
Alex <alex.chart@turningpointhr.com>
2021-11-26 20:51:12 +0000

    commit tree dc613320808c606fa00acdf1a5a6b134539becf0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(driversArray){
+    return driversArray.slice(0,2);
+}
+function returnLastTwoDrivers(driversArray){
+    return driversArray.slice(2,5);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer){
+    return function(){
+    return integer*integer
+    }
+}
+// function fareDoubler(createFareMultiplier){
+//     return createFareMultiplier.fareDoubler*2;
+// } keep this in as one test was passing and was close
+
+const fareDoubler = fare => fare*2
+const fareTripler = fare => fare*3
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers);
+}
